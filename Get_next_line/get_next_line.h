101d2057d1566a06039f6356a9cac897d7a9fd3e
Felix-junior FRIMPONG <ffrimpon@e3r7p15.42.fr>
2016-09-21 18:10:30 +0200

    Repository copied correctly. No submodule

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffrimpon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/14 14:01:46 by ffrimpon          #+#    #+#             */
+/*   Updated: 2016/02/03 23:43:20 by ffrimpon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+# define BUFF_SIZE 1
+
+int	get_next_line(const int fd, char **line);
+
+#endif
